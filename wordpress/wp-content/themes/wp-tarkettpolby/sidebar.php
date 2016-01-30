20d42a92eed070c8ed42a65807dbceebb50038d1
crazyyy <crazyyy@gmail.com>
2016-01-31 00:55:26 +0200

    wordpressed

---
+++ wordpress/wp-content/themes/wp-tarkettpolby/sidebar.php
@@ -0,0 +1,47 @@
+<aside class="sidebar col-md-3" role="complementary">
+  <div class="row">
+    <div class="widget widget-catalog col-md-12 col-xs-6 col-sm-6">
+      <h6>КАТАЛОГ</h6>
+      <ul>
+        <li><a href="#">СЕТКА РАБИЦА</a></li>
+        <li><a href="#">ЖИДКИЕ ОБОИ</a></li>
+        <li><a href="#">КОВРОЛИН</a></li>
+        <li><a href="#">КОВРЫ</a></li>
+        <li><a href="#">ЛАМИНАТ</a></li>
+        <li><a href="#">ЛИНОЛЕУМ</a></li>
+        <li><a href="#">ГЕТЕРОГЕННОЕ ГОМОГЕННОЕ ПВХ ПОКРЫТИЯ</a></li>
+        <li><a href="#">ПЛИНТУС</a></li>
+        <li><a href="#">ПОДЛОЖКА</a></li>
+        <li><a href="#">ПОРОГИ АЛЮМИНИЕВЫЕ</a></li>
+        <li><a href="#">ОСБ ПЛИТЫ</a></li>
+        <li><a href="#">МДФ ПАНЕЛИ</a></li>
+        <li><a href="#">ПВХ ПАНЕЛИ</a></li>
+        <li><a href="#">КЛЕЙ</a></li>
+        <li><a href="#">ДВУХСТОРОННИЙ СКОТЧ</a></li>
+      </ul>
+    </div><!-- widget widget-catalog-->
+    <div class="widget widget-news col-md-12 col-xs-6 col-sm-6">
+      <h6>НОВОСТИ:</h6>
+      <h3 class="title-red">СНИЖЕНИЕ ЦЕН НА КОВРОЛИН ЛИНОЛЕУМ И ЛАМИНАТ</h3>
+      <h4 class="title-subtitle">ТЕПЕРЬ ЦЕНЫ ЕЩЕ НИЖЕ</h4>
+      <ul>
+        <li>
+          <h3><a href="#">ЛАМИНАТ КРОНОШПАН FORTE 33 класс</a></h3>
+          <span class="price-new">120 000</span> / m²
+        </li>
+        <li>
+          <h3><a href="#">КОВРОЛИН АТЛАНТ</a></h3>
+          <span class="price-new">94 000</span> / m²
+        </li>
+        <li>
+          <h3><a href="#">КОВРОЛИН ГЛОБАЛ</a></h3>
+          <span class="price-new">от 94 000</span> / m²
+        </li>
+        <li>
+          <h3><a href="#">КОВРОЛИН ЛИДЕР</a></h3>
+          <span class="price-new">130 000</span> / m²
+        </li>
+      </ul>
+    </div><!-- /.widget widget-news -->
+  </div>
+</aside><!-- /sidebar -->
